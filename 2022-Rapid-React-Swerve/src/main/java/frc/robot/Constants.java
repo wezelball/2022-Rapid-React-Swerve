7f86b40b04e5f96b6cb730272038ac71aa66a004
= <=>
2022-04-20 17:44:25 -0400

    Trying to scale encoder velocities and angles, brain is on fire

--- 2022-Rapid-React-Swerve/src/main/java/frc/robot/Constants.java
+++ 2022-Rapid-React-Swerve/src/main/java/frc/robot/Constants.java
@@ -1,10 +1,21 @@
 package frc.robot;
 
+import edu.wpi.first.math.util.Units;
+
 public class Constants {
     private static final double kWheelRadius = 0.0508;  // Supply real values when available (looks like 4"?)
-    private static final int kEncoderResolution = 4096; // This looks right, but double-check
+    public static final int kEncoderResolution = 2048; // This looks right, but double-check
 
     private static final double kModuleMaxAngularVelocity = Drivetrain.kMaxAngularSpeed;
     private static final double kModuleMaxAngularAcceleration = 2 * Math.PI; // radians per second squared - how was this derived?
-    
+ 
+    public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
+    public static final double kDriveMotorGearRatio = 1 / 6.75;     // for SDS
+    public static final double kTurningMotorGearRatio = 1 / 12.8;   // for SDS
+    public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
+    public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2 * Math.PI;
+    public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
+    public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
+    public static final double kPTurning = 0.5;
+
 }
--- 2022-Rapid-React-Swerve/src/main/java/frc/robot/Drivetrain.java
+++ 2022-Rapid-React-Swerve/src/main/java/frc/robot/Drivetrain.java
@@ -21,9 +21,9 @@ public class Drivetrain {
     private final Translation2d m_backLeftLocation = new Translation2d(-0.381, 0.381);      // Supply real values when available
     private final Translation2d m_backRightLocation = new Translation2d(-0.381, -0.381);    // Supply real values when available
 
-    private final SwerveModule m_frontLeft = new SwerveModule(1, 2);
-    private final SwerveModule m_frontRight = new SwerveModule(3, 4);
-    private final SwerveModule m_backLeft = new SwerveModule(5, 6);
-    private final SwerveModule m_backRight = new SwerveModule(7, 8);
+    //private final SwerveModule m_frontLeft = new SwerveModule(1, 2);
+    //private final SwerveModule m_frontRight = new SwerveModule(3, 4);
+    //private final SwerveModule m_backLeft = new SwerveModule(5, 6);
+    //private final SwerveModule m_backRight = new SwerveModule(7, 8);
     
 }
